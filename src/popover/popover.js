b507942260ded8f553ea478a95d2db20bde12188
Carlos Martin <carlos@Carlos-Pro.local>
2014-05-28 18:13:49 -0600

    Adding popoverHtmlUnsafe

--- src/popover/popover.js
+++ src/popover/popover.js
@@ -17,3 +17,16 @@ angular.module( 'ui.bootstrap.popover', [ 'ui.bootstrap.tooltip' ] )
 .directive( 'popover', [ '$tooltip', function ( $tooltip ) {
   return $tooltip( 'popover', 'popover', 'click' );
 }]);
+
+.directive( 'popoverHtmlUnsafePopup', function () {
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&' },
+    templateUrl: 'template/popover/popover-html-unsafe-popup.html'
+  };
+})
+
+.directive( 'popoverHtmlUnsafe', [ '$tooltip', function ( $tooltip ) {
+  return $tooltip( 'popoverHtmlUnsafe', 'popover', 'mouseenter' );
+}])
